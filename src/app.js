c230cd10f1653162869f5ad662565dcf3d4a40d6
cf9874 <cf9874@fgb.ai>
2022-08-18 11:15:30 +0900

    test setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { Container } from "pixi.js";
 import ViewManager from "./components/ViewManager";
 import UIUtil from "./components/UIUtil";
+import LoadingImage from "./components/LoadingImage";
 
 export default class App {
   constructor() {
@@ -13,7 +14,17 @@ export default class App {
       color: 0xd0a040,
       line: true,
     });
+    const BoxContainer2 = UIUtil.makeRec({
+      x: 300,
+      y: 50,
+      width: 600,
+      height: 800,
+      color: 0xd0a040,
+      line: true,
+    });
     this.view = new ViewManager();
-    this.con.addChild(BoxContainer, this.view.con);
+    this.Loading = new LoadingImage();
+    // this.con.addChild(BoxContainer, this.view.con);
+    this.con.addChild(BoxContainer2, this.Loading.con);
   }
 }
